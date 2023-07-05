f572b9221ea80ca247331afb0115a485c33e3e43
funnymonke12 <funnimonke12@gmail.com>
2023-07-05 09:35:53 +0500

    Setup project

--- create_trello.py
+++ create_trello.py
@@ -1,16 +1,7 @@
-
-
-
-
+from config import apiKey, apiToken
 def connect_trello():
     import requests
-    import json
-
-    apiKey = '0cfd11dfa4ca603b05a6e2791d7defbd'
-    apiToken = 'ATTA84a62b8ba4a1f27b85e6ed6ccf51a5f1a22fd35d2177a3b7476d7be067fc0bd2B1E4724E'
-
     url = 'https://api.trello.com/1/cards/'
-
 
     headers = {
       "Accept": "application/json"
